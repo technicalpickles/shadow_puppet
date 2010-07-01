f5ef7564467e2c9f963a089bdb8940981e052278
Joshua Nichols <josh@technicalpickles.com>
2010-07-01 17:35:55 -0400

    Switched to isolate-scenarios.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,7 @@
 require 'rubygems'
 require 'ruby-debug'
-require 'isolate'
 
-sandbox = Isolate::Sandbox.new do
-  gem 'puppet', '= 0.24.8'
-  gem 'facter', '>= 1.5.4'
-  gem 'highline', '>= 1.5.0'
-  gem 'builder', '>= 2.1.2'
-  gem 'activesupport', "= #{ENV['ACTIVESUPPORT_VERSION'] || '2.3.8'}"
-
-  gem 'rspec', '~> 1.3.0'
-end
-
-sandbox.activate
+require 'isolate/scenarios/now'
 
 require 'spec'
 require 'spec/autorun'
@@ -24,5 +13,3 @@ require 'shadow_puppet/test'
 Dir.glob(File.join(File.dirname(__FILE__), 'fixtures', '*.rb')).each do |manifest|
   require manifest
 end
-
-puts "Testing against activesupport-#{ActiveSupport::VERSION::STRING}"
